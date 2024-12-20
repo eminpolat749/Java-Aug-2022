866512aad6ce16b2963d4aaa828076e9135b0833
Emin Polat <51139204+eminpolat749@users.noreply.github.com>
2024-12-20 16:33:08 +0300

    updated

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,9 +1,11 @@
 package org.csystem.app;
 
+import static org.csystem.app.Application.run;
+
 class App {
 	public static void main(String [] args)
 	{
-
+		run(args);
 	}
 }
 
--- src/Sample/src/org/csystem/app/Application.java
+++ src/Sample/src/org/csystem/app/Application.java
@@ -9,26 +9,7 @@ import java.nio.ByteBuffer;
 public class Application {
     public static void run(String[] args)
     {
-        for (;;) {
-            short a = Console.readShort("Bir sayı giriniz:");
-
-            ByteBuffer byteBuffer = ByteBuffer.allocate(2).putShort(a);
-
-            Console.writeLine(SystemUtil.endianText(byteBuffer));
-            SystemUtil.changeEndian(byteBuffer);
-            Console.writeLine(SystemUtil.endianText(byteBuffer));
-            short b = BitConverter.toLittleEndian(a);
-
-            Console.writeLine("a = %d, a = %04X", a, a);
-            Console.writeLine("b = %d, b = %04X", b, b);
-
-            if (a == 0)
-                break;
-        }
+        System.out.println("Hello World");
     }
 
-    public static void main(String[] args)
-    {
-        run(args);
-    }
 }
